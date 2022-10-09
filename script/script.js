3c57c397019d49ceb75bbd3b821a17949db264e4
WebH <max@webh.dev>
2022-10-09 13:30:48 +0100

    -

--- script/script.js
+++ script/script.js
@@ -4,6 +4,30 @@ const menuButton = document.querySelector(".menu");
 const navBar = document.querySelector(".nav");
 
 menuButton.addEventListener('click', () => {
-    navBar.classList.toggle("show-nav");
-    console.log("I clicked");
+    if (navBar.classList.contains("show-nav")) {
+        navBar.classList.remove("show-nav");
+        navBar.classList.add("hide-nav");
+    } else {
+        navBar.classList.add("show-nav");
+        navBar.classList.remove("hide-nav");
+    }
+
+})
+
+const items = document.querySelectorAll(".item");
+
+const observer = new IntersectionObserver(
+    entries => {
+        entries.forEach(entry => {
+            entry.target.classList.toggle("show-item", entry.isIntersecting)
+            if (entry.isIntersecting) observer.unobserve(entry.target)
+        })
+    },
+    {
+        threshold: 0.5
+    }
+)
+
+items.forEach(item => {
+    observer.observe(item)
 })
