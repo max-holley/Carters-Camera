a9ec8383ec11e4944e9d7a527af334badcac94a2
WebH <max@webh.dev>
2022-10-09 09:28:32 +0100

    -

--- script/script.js
+++ script/script.js
@@ -1,1 +1,9 @@
 console.log("hello there");
+
+const menuButton = document.querySelector(".menu");
+const navBar = document.querySelector(".nav");
+
+menuButton.addEventListener('click', () => {
+    navBar.classList.toggle("show-nav");
+    console.log("I clicked");
+})
